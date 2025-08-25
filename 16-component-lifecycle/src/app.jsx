0be7bd2e843464d261f596c7172e498e4f9d27c7
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-25 11:36:11 +0900

    learn. 언마운트 시 상태 유실, 마운트 상태 초기화 - 상태 유지를 위해서 상태 끌어올리기 전략

--- 16-component-lifecycle/src/app.jsx
+++ 16-component-lifecycle/src/app.jsx
@@ -3,25 +3,36 @@ import { LearnSection } from '@/components'
 
 export default function App() {
   console.log('App 렌더링')
+
   const [isVisible, setIsVisible] = useState(true)
   const handleInput = (e) => setIsVisible(e.target.checked)
+
+  const [inputValue, setInputValue] = useState('Child 컴포넌트')
+  const [headline, setHeadline] = useState('사자 보이즈')
+  const updateHeadline = () => setHeadline((h) => h + '🦁')
 
   return (
     <LearnSection
       className="p-10"
-      title="컴포넌트 라이프사이클(생명주기: 탄생(mount) -> 성장(update) -> 죽음(unmount)"
+      title="컴포넌트 라이프사이클(생명주기: 탄생(mount) -> 성장(update) -> 죽음(unmount))"
     >
       <label className="flex gap-1 items-center">
         <input
           type="checkbox"
           name="is-visible"
-          aria-label="Child 컴포넌트 표시"
           checked={isVisible}
           onChange={handleInput}
         />
         Child 컴포넌트 표시 ({isVisible.toString()})
       </label>
-      {isVisible ? <Child /> : null}
+      {isVisible ? (
+        <Child
+          headline={headline}
+          updateHeadline={updateHeadline}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
+      ) : null}
     </LearnSection>
   )
 }
@@ -31,18 +42,19 @@ export default function App() {
 // 2. 변경(update) x N (여러 번 발생할 수 있음)
 // 0. 소멸(unmount)
 // 어떤 특정한 객체가 시간의 흐름에 따라 변경하는 것을 라이프 사이클이라고 하는데, 그 단계는 생성, 변경, 소멸이라고 말할 수 있다.
-function Child() {
+function Child({ headline, updateHeadline, inputValue, setInputValue }) {
   console.log('Child 렌더링')
 
-  const [headline, setHeadline] = useState('Child 컴포넌트')
   return (
     <article className="mt-5 p-5 border-2 border-inherit">
-      <h2>{headline}</h2>
-      <button
-        type="button"
-        className="button"
-        onClick={() => setHeadline((h) => h + '🦁')}
-      >
+      <h2 className="text-xl font-extrabold mb-2">{headline}</h2>
+      <input
+        type="text"
+        className="input"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="button" className="button mt-2" onClick={updateHeadline}>
         사자 이모지 추가
       </button>
     </article>
