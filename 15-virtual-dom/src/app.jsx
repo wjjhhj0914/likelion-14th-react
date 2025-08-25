2797623628d63ad658d050cd382de8b3e44b3c0d
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-25 09:13:48 +0900

    init. 리액트 프로젝트 템플릿 구성

---
+++ 15-virtual-dom/src/app.jsx
@@ -0,0 +1,9 @@
+import { LearnSection } from '@/components'
+
+export default function App() {
+  return (
+    <LearnSection title="가상 DOM 작동 방식 이해" className="m-10">
+      <p>가상 돔의 작동 구조에 대한 이해</p>
+    </LearnSection>
+  )
+}
