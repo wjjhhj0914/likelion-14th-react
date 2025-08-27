bfa652b56abd7fd917967655f3c4d0ab5152c837
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-27 14:56:54 +0900

    learn. 데이터 가져오기 - 이전 요청 중단 - AbortController - controller.signal - controller.abort()

--- 20-fetching-data/src/app.jsx
+++ 20-fetching-data/src/app.jsx
@@ -43,14 +43,102 @@ export default function App() {
         <output>렌더링 키: {key}</output>
       </div>
 
-      <Album id={albumId} />
+      <AlbumAbortDemo id={albumId} />
     </LearnSection>
   )
 }
 
 const ALBUM_API_URL = 'https://jsonplaceholder.typicode.com/albums'
 
-function Album({ id }) {
+// --------------------------------------------------------------------------
+// 중복 요청 제거 데모
+// - 중단(Abort) 컨트롤러 활용
+// - AbourtController 클래스
+// - AbourtController 인스턴스 생성 (예: const controller = new AbortController())
+// - AbourtController 인스턴스는 속성(controller.signal)과 메서드(controller.abort())
+
+function AlbumAbortDemo({ id }) {
+  console.log(`Album ${id} 렌더링`)
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    setError(null)
+
+    // 요청 중단할 수 있는 AbortController 인스턴스(객체)
+    const abortController = new AbortController()
+
+    fetch(ALBUM_API_URL + '/' + id, {
+      // 요청 중단을 위해 생성된 abortController의
+      // 시그널(signal, 신호)를 fetch 요청의 옵션에 연결(설정)
+      signal: abortController.signal,
+    })
+      .then(async (response) => {
+        await wait(0.4)
+        if (response.ok) {
+          return response.json()
+        }
+
+        if (response.status === 404) {
+          throw new Error('API 요청에 따른 응답된 데이터를 찾을 수 없습니다.')
+        }
+      })
+      .then((responseData) => {
+        console.log('데이터 가져오기 -> data 상태 업데이트')
+        setData(responseData)
+      })
+      .catch((error) => {
+        if (error.name === 'AbortError') return
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+    return () => {
+      // 중복된(이전) 요청 취소
+      abortController.abort()
+    }
+  }, [id])
+
+  if (loading) {
+    return (
+      <p
+        role="status"
+        aria-live="polite"
+        className="text-indigo-300 font-semibold text-2xl"
+      >
+        로딩 중...
+      </p>
+    )
+  }
+
+  if (error) {
+    return (
+      <p
+        role="alert"
+        aria-live="assertive"
+        className="text-red-600 font-semibold text-2xl"
+      >
+        오류 발생!! {error.message}
+      </p>
+    )
+  }
+
+  return (
+    <p className="text-indigo-600 font-semibold text-2xl">
+      앨범 타이틀 : {data?.id ?? 0} | {data?.title ?? 'Album Title'}
+    </p>
+  )
+}
+
+// --------------------------------------------------------------------------
+// 이전 요청 무시 데모
+
+function AlbumIgnoreDemo({ id }) {
   console.log(`Album ${id} 렌더링`)
 
   // 데이터 가져오기(fetching data) 상태 관리
@@ -69,16 +157,15 @@ function Album({ id }) {
   // - rendering [2]
   // - cleanup ([1]: ignore = true)
   // - remount [2]
-  // - response ([1]: ignore = true) - view update ignored
+  // - response ([1]: ignore = true) - view update ignore
   // - effect ([2]: ignore = false)
-  // - response ([2]: ignore = true) - view update
+  // - response ([2]: ignore = false) - view update
   // 상위 컴포넌트에서 전달된 id 속성이 변경되면 다시 이펙트 함수 실행
   useEffect(() => {
     // 이펙트 함수 내부에
-    // 1회 요청 무시 여부를 식별하는 지역 변수 선언
-    const ignore = false
+    // 1회 요청을 무시 여부를 식별하는 지역 변수 선언
+    let ignore = false
 
-    // 상태 변경은 일괄 처리(업데이트)
     // 로딩 상태 전환
     setLoading(true)
     // 에러 상태 전환
@@ -101,11 +188,12 @@ function Album({ id }) {
       .then((responseData) => {
         // 데이터 업데이트
         // 개발 중, 엄격 모드에서 이펙트가 2회 실행되다 보니
-        // 데이터 상태 업데이트도 2회 실행된다. (이 점을 문제 삼을 수 있다.)
-        // 그렇다면, 2회 네트워크 요청(데이터 가져오기)를 하더라도
-        // 실제 데이터 상태 업데이트 반영은 1회로 제한할 수 없을까? (라는 생각을 해볼 수 있다.)
-        // 1회 요청 -> 2회 요청
-        // 1회 데이터 상태 업데이트(무시) -> 2회 데이터 상태 업데이트(적용)
+        // 데이터 상태 업데이트도 2회 실행된다.
+        // 자... 그렇다면? 2회 네트워크 요청(데이터 가져오기)를 하더라도
+        // 실제 데이터 상태 업데이트 반영은 1회로 제한할 수 없을까?
+        // - 1회 요청 -> 2회 요청
+        // - 1회 데이터 상태 업데이트(무시: ignore) -> 2회 데이터 상태 업데이트(적용)
+
         if (!ignore) {
           console.log('데이터 가져오기 -> data 상태 업데이트')
           setData(responseData)
@@ -122,10 +210,10 @@ function Album({ id }) {
         setLoading(false)
       })
 
-    // 클린업(정리) 함수를 사용해서
+    // 클린업(정리) 함수
     return () => {
       // 무시(ignore) 변수 값을 "1회 요청을 무시하라!"로 변경
-      // ignore = true
+      ignore = true
     }
   }, [id])
 
