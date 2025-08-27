db00eae57601a4f3b6eb6ba45cf4d51eaf4bf7e1
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-27 11:23:38 +0900

    learn. 데이터 가져오기(fetching data) 상태 관리
    - 로딩(loading | pending)
    - 에러(error)
    - 데이터(data)

--- 20-fetching-data/src/app.jsx
+++ 20-fetching-data/src/app.jsx
@@ -1,10 +1,63 @@
+import { useEffect, useState } from 'react'
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
 
 export default function App() {
+  console.log('App 렌더링')
+
+  const [key, setKey] = useState(0)
+
+  // 데이터 가져오기(fetching data) 상태 관리
+  // 1. 로딩(loading | pending)
+  const [loading, setLoading] = useState(false)
+  // 2. 에러(error)
+  const [error, setError] = useState(null)
+  // 3. 데이터(data)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    // 로딩 상태로 전환
+    setLoading(true)
+    // 에러 상태 전환 (초기화)
+    setError(null)
+
+    // 리액트 렌더링은 항상 동기 방식으로 작동
+    // 리액트 렌더링과 무관한 서버에서 데이터 가져오기 코드
+    fetch('https://jsonplaceholder.typicode.com/albums/1')
+      .then((response) => response.json())
+      .then((responseData) => {
+        // 데이터 업데이트
+        setData(responseData) // null => data로 응답이 업데이트될 것.
+
+        // 로딩 상태 전환
+        setLoading(false)
+      })
+      .catch((error) => {
+        // 에러 상태 전환
+        // alert(error.message)
+        setError(error)
+      })
+  }, [])
+
+  console.log({ loading, error, data })
+
+  // 조건부 렌더링 (화면에 표시)
+  // - 조건문 사용
+  // - 조건식 사용
+  // - JSX 내부에 조건식 사용
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection title="데이터 가져오기(fetching Data)" showTitle>
+      <p>{'앨범 이름'}</p>
+      <div role="group" className="mt-5">
+        <button
+          type="button"
+          className="button"
+          onClick={() => setKey((k) => k + 1)}
+        >
+          렌더링 키 변경
+        </button>
+        <output>렌더링 키: {key}</output>
+      </div>{' '}
     </LearnSection>
   )
 }
--- 20-fetching-data/src/main.jsx
+++ 20-fetching-data/src/main.jsx
@@ -6,4 +6,8 @@ import './styles/main.css'
 const root = document.getElementById('root')
 if (!root) throw new Error('문서에 #root 요소가 존재하지 않습니다.')
 
-createRoot(root).render(<App />)
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
