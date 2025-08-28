ab7df22bb7f2cba6e96405fa70163823a57a6a51
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-28 10:49:11 +0900

    learn. 사용자 프로필 테이블 실습 (접근성 준수)
    - 상태 관리
    - 부수 효과
    - 부수 효과 정리
    - 의미 있는 구조화
    - 누구나 접근가능한 표 설계

---
+++ 21-user-profile-table/src/app.jsx
@@ -0,0 +1,280 @@
+// 야무쌤의 솔루션
+import { useEffect, useState } from 'react'
+import { LearnSection } from '@/components'
+import { wait } from './utils'
+
+export default function App() {
+  return (
+    <LearnSection title="사용자 프로필 테이블" className="p-10">
+      <UserProfileTable />
+    </LearnSection>
+  )
+}
+
+function UserProfileTable() {
+  // pending, error, data 상태 선언
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+
+  // 부수 효과 (서버에서 데이터 가져오기)
+  // - axios / Fetch API
+  // - Promise / 비동기(async) 함수 + try...catch
+  // 부수 효과 정리
+  // - 이전 요청에 따른 상태 업데이트 무시
+  // - 이전 요청 중단
+
+  useEffect(() => {
+    // 상태 업데이트
+    setIsPending(true)
+    setError(null)
+
+    // 이전 요청 중단
+    const controller = new AbortController()
+
+    // 이전 요청 무시
+    let ignore = false
+
+    async function fetchUsers() {
+      try {
+        await wait(1.2)
+
+        const response = await fetch('http://localhost:4000/users', {
+          signal: controller.signal,
+        })
+
+        if (!response.ok && response.status === 404) {
+          throw new Error('데이터베이스에 사용자 정보가 없습니다.')
+        }
+
+        const userData = await response.json()
+
+        // 이전 요청 무시
+        if (!ignore) {
+          console.log('데이터 가져오기 -> 뷰 렌더링')
+          setData(userData)
+        }
+      } catch (error) {
+        if (error.name === 'AbortError') return
+        setError(error)
+      } finally {
+        setIsPending(false)
+      }
+    }
+
+    fetchUsers()
+
+    return () => {
+      ignore = true
+      controller.abort()
+    }
+  }, [])
+
+  // 조건부 렌더링
+  // - pending 상태일 때
+  // - error 상태일 때
+  // - data 응답 결과가 있을 때
+
+  if (isPending) {
+    return (
+      <div role="status" aria-live="polite" className="text-2xl text-sky-600">
+        사용자 데이터 로딩 중...
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div
+        role="alert"
+        aria-live="assertive"
+        className="flex flex-col gap-2 text-2xl bg-red-600 text-red-50 p-4 fixed top-0 left-0 right-0"
+      >
+        <h3 className="text-xl font-semibold">{error.name}</h3>
+        <p className="text-base">{error.message} 추후 다시 시도해주세요.</p>
+      </div>
+    )
+  }
+
+  return (
+    <table className="min-w-200 border-2 border-sky-900">
+      <caption className="sr-only">사용자 프로필</caption>
+      <thead>
+        <tr>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            이름
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            아이디
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            이메일
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            도시
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            회사명
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            슬로건
+          </th>
+          <th scope="col" className="p-1 border-1  border-sky-900 bg-sky-50">
+            주요사업
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map(({ id, name, username, email, address, company }) => {
+          return (
+            <tr key={id}>
+              <td className="p-1 border-1 text-center border-sky-900">
+                {name}
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                <code>{username}</code>
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                <a
+                  href={`email:${email}`}
+                  className="text-sky-600 hover:text-sky-700"
+                >
+                  {email}
+                </a>
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                {address.city}
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                {company.name}
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                {company.catchPhrase}
+              </td>
+              <td className="p-1 border-1 text-center border-sky-900">
+                {company.business}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+// My Solution
+// import { useEffect, useState } from 'react'
+// import { LearnSection } from '@/components'
+// import { wait } from './utils'
+
+// export default function App() {
+//   return (
+//     <LearnSection title="데이터 가져오기(fetching data)" showTitle>
+//       <UsersTable />
+//     </LearnSection>
+//   )
+// }
+
+// const API = 'http://localhost:4000'
+
+// function UsersTable() {
+//   const [loading, setLoading] = useState(false)
+//   const [error, setError] = useState(null)
+//   const [users, setUsers] = useState([])
+
+//   useEffect(() => {
+//     // 이전 요청 중단
+//     const controller = new AbortController()
+
+//     // 이전 요청 무시
+//     let ignore = false
+
+//     ;(async () => {
+//       try {
+//         // 1. 상태 업데이트
+//         setLoading(true)
+//         setError(null)
+
+//         // 지연 처리
+//         await wait(1.2)
+
+//         const response = await fetch(`http://localhost:4000/users`, {
+//           signal: controller.signal,
+//         })
+//         if (!response.ok && response.status === 404)
+//           throw new Error(`API 요청에 따른 응답된 데이터를 찾을 수 없습니다.`)
+
+//         const data = await response.json()
+
+//         // 이전 요청 무시
+//         if (!ignore) setUsers(data)
+//       } catch (err) {
+//         if (err.name === 'AbortError') return
+//         setError(err)
+//       } finally {
+//         setLoading(false)
+//       }
+//     })()
+
+//     return () => {
+//       ignore = true
+//       controller.abort()
+//     }
+//   }, [])
+
+//   if (loading)
+//     return (
+//       <p
+//         role="status"
+//         aria-live="polite"
+//         className="text-indigo-300 font-semibold text-2xl"
+//       >
+//         로딩 중...
+//       </p>
+//     )
+
+//   if (error)
+//     return (
+//       <p
+//         role="alert"
+//         aria-live="assertive"
+//         className="text-red-600 font-semibold text-2xl"
+//       >
+//         오류 발생!! {error.message}
+//       </p>
+//     )
+
+//   return (
+//     <table>
+//       <caption>사용자 프로필</caption>
+//       <thead>
+//         <tr>
+//           <th scope="col">이름</th>
+//           <th scope="col">아이디</th>
+//           <th scope="col">이메일</th>
+//           <th scope="col">도시</th>
+//           <th scope="col">회사명</th>
+//           <th scope="col">슬로건</th>
+//           <th scope="col">주요사업</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {users.map((u) => (
+//           <tr key={u.id}>
+//             <td>{u.name}</td>
+//             <td>
+//               <code>{u.username}</code>
+//             </td>
+//             <td>
+//               <a href={`mailto:${u.email}`}>{u.email}</a>
+//             </td>
+//             <td>{u.address?.city}</td>
+//             <td>{u.company?.name}</td>
+//             <td>{u.company?.catchPhrase}</td>
+//             <td>{u.company?.business}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   )
+// }
