26a98faf2f72ad4684e45f93b082a3f69c2dfbd3
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-29 16:08:54 +0900

    learn. useRef 훅과 타이머 작동

--- 26-value-ref/src/app.jsx
+++ 26-value-ref/src/app.jsx
@@ -1,22 +1,71 @@
 import { Component, useState } from 'react'
 import { LearnSection } from '@/components'
 
+// 사용자 정의 훅
+function useRef(initialValue) {
+  // 리액트의 빌트인(내장) 훅 사용 ✅
+  const [ref] = useState({ current: initialValue })
+  return ref
+}
+
 export default function App() {
   return (
     <LearnSection
       title="클래스 vs. 함수 값 참조"
       className="bg-slate-950 h-screen flex flex-col gap-4"
     >
-      <MemoPrevStateValue />
+      <TimerDemo />
+      {/* <MemoPrevStateValue /> */}
       {/* <FunctionalComponent /> */}
       {/* <ClassComponent /> */}
     </LearnSection>
   )
 }
 
-function useRef(initialValue) {
-  const [ref] = useState({ current: initialValue })
-  return ref
+// --------------------------------------------------------------------------
+
+// 타이머 추가/제거 (타이머 추가 및 제거하기 위한 타이머 ID 기억)
+// 다음 번에 렌더링 되더라도 값을 기억해야 타이머를 제거할 수 있음
+
+function TimerDemo() {
+  const [timer, setTimer] = useState(null)
+  const timerIdRef = useRef() // { current: 3 }
+
+  const handleStartTimer = () => {
+    ;(function recursion() {
+      clearTimeout(timerIdRef.current)
+      timerIdRef.current = setTimeout(() => {
+        console.log('타이머 작동 중...')
+        setTimer(new Date())
+        recursion()
+      }, 1000)
+    })()
+  }
+
+  const handleStopTimer = () => {
+    console.log('타이머 중지!')
+    clearTimeout(timerIdRef.current)
+    setTimer(null)
+  }
+
+  return (
+    <section
+      aria-label="타이머 데모"
+      className="bg-white text-black p-20 text-2xl flex flex-col gap-10"
+    >
+      <div role="group" className="space-y-3">
+        <button type="button" className="button" onClick={handleStartTimer}>
+          타이머 시작
+        </button>
+        <button type="button" className="button" onClick={handleStopTimer}>
+          타이머 중지
+        </button>
+      </div>
+      {timer && (
+        <time dateTime={timer.toISOString()}>{timer.toLocaleTimeString()}</time>
+      )}
+    </section>
+  )
 }
 
 // 이전 상태 값 기억 예시
